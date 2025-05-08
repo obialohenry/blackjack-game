ca9ccf80356d169d704907abc2dab15efea0dcf9
sisu <obilaorchisomebi123@gmail.com>
2025-05-08 12:13:47 +0100

    Add comments to functions; Check for ace in list stand hit black jack
    This comments are to describe what each of these functions do.

--- task/main.py
+++ task/main.py
@@ -5,6 +5,10 @@ cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 # continue_playing = True
 
 def check_for_ace_in_list(item:list):
+    """To Check if there is an ACE in a hand[being a List of cards].
+    If there is an ACE, and the total card at hand is above 21 it
+    changes the ACE from 11 to 1.
+    """
     for item_object in item:
         if item_object == 11:
             ace_index = item.index(item_object)
@@ -12,7 +16,17 @@ def check_for_ace_in_list(item:list):
                 item[ace_index] = 1
 
 def stand(dealers_cards:list,my_cards:list)->bool:
-
+    """When the player chooses to stand[To not chose another card],
+       the total cards for both dealer and player are calculated taking into
+       consideration if there is an ACE in either cards at both hands, and the
+       game rules. Rules involving;
+       - Adding an extra card to the dealer's cards if it's sum total is not up
+         to 17 and above.
+       - Taking preference of when either cards on both hands are above 21, and
+         if dealers card is 21 in total[A Blackjack].
+       - The cards with the highest total, when both cards at hand are below 21,
+         or when dealers total card is not up to 21, wins.
+    """
     check_for_ace_in_list(dealers_cards)
     check_for_ace_in_list(my_cards)
 
@@ -47,7 +61,13 @@ def stand(dealers_cards:list,my_cards:list)->bool:
 
 
 def hit(my_cards:list,dealers_cards:list)-> bool:
-
+    """When a player chose to continue playing,by adding a card
+       to his/her stack.
+       here we check for;
+       - If players total cards exceeds 21, game ends, and
+         player loses.
+       - if not, game continues.
+     """
     random.shuffle(cards)
     my_cards.append(random.choice(cards))
 
@@ -66,6 +86,13 @@ def hit(my_cards:list,dealers_cards:list)-> bool:
     return True
 
 def black_jack():
+    """This is the game itself. It starts by selecting and adding
+       two random cards from the card list, to both dealers hand,
+       and players hand. Then the cards of the player is displayed, with
+       the first card of the dealer, and the dealer is then asked to either
+       HIT or STAND.
+
+    """
     my_cards = []
     dealers_cards = []
     random.shuffle(cards)
